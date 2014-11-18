ee83a9a9c7ed1265b605af1caaf433e0b3b504fd
chaostone <duantihua@gmail.com>
2014-11-19 07:23:03 +0800

    change securitry scope with derived value

--- blueprint/src/main/scala/org/beangle/security/blueprint/nav.scala
+++ blueprint/src/main/scala/org/beangle/security/blueprint/nav.scala
@@ -1,18 +1,21 @@
 package org.beangle.security.blueprint
 
-import org.beangle.data.model.{ Enabled, Hierarchical, IntIdEntity, Named }
-import org.beangle.data.model.SlowId
+import org.beangle.data.model.{ Enabled, Hierarchical, IntIdEntity, Named, SlowId }
 
 trait MenuProfile extends IntIdEntity with Named with Enabled with SlowId {
   def menus: Seq[Menu]
   def role: Role
 }
 
-trait Menu extends IntIdEntity with Named with Enabled with Hierarchical[Menu] with SlowId {
-
+trait Menu extends IntIdEntity with Named with Enabled with Hierarchical[Menu] with Ordered[Menu] with SlowId {
+  def profile: MenuProfile
   def title: String
-  def entry: Option[FuncResource]
+  def entry: FuncResource
   def params: String
   def remark: String
-  def resources: Seq[FuncResource]
+  def resources: collection.Set[FuncResource]
+
+  override def compare(other: Menu): Int = {
+    indexno.compareTo(other.indexno)
+  }
 }
