c0e99b35d236b676b20b09eed3752957a0bda1e4
chaostone <duantihua@gmail.com>
2017-03-23 13:25:03 +0800

    add WebSecurityManager

---
+++ web/src/main/scala/org/beangle/security/web/WebSecurityManager.scala
@@ -0,0 +1,35 @@
+package org.beangle.security.web
+
+import org.beangle.security.authc.AuthenticationToken
+import org.beangle.security.authz.Authorizer
+import org.beangle.security.session.SessionRegistry
+import org.beangle.security.session.Session
+import org.beangle.security.authc.Authenticator
+import org.beangle.security.web.session.SessionIdPolicy
+import org.beangle.security.mgt.SecurityManager
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
+import org.beangle.security.web.authc.WebClient
+
+class WebSecurityManager(val authenticator: Authenticator, val authorizer: Authorizer,
+                         val sessionRegistry: SessionRegistry, val sessionIdPolicy: SessionIdPolicy)
+    extends SecurityManager {
+
+  override def login(sessionId: String, token: AuthenticationToken, client: Session.Client): Session = {
+    sessionRegistry.register(sessionId, authenticator.authenticate(token), client)
+  }
+
+  def login(request: HttpServletRequest, response: HttpServletResponse, token: AuthenticationToken): Session = {
+    val key = sessionIdPolicy.newSessionId(request, response)
+    sessionRegistry.register(key, authenticator.authenticate(token), WebClient.get(request))
+  }
+
+  def logout(request: HttpServletRequest, response: HttpServletResponse,
+             session: Session): Unit = {
+    session.stop()
+    sessionIdPolicy.delSessionId(request, response)
+    val s = request.getSession(false)
+    if (null != s) s.invalidate()
+  }
+
+}
