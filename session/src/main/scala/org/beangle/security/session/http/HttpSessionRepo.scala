87ce0f73f97fd7d8bad22eb318b2a5ff2d6afcb0
chaostone <duantihua@gmail.com>
2017-08-23 10:43:43 +0800

    rebase new serialize api

--- session/src/main/scala/org/beangle/security/session/http/HttpSessionRepo.scala
+++ session/src/main/scala/org/beangle/security/session/http/HttpSessionRepo.scala
@@ -3,19 +3,20 @@ package org.beangle.security.session.http
 import java.net.{ HttpURLConnection, URL }
 
 import org.beangle.cache.CacheManager
-import org.beangle.commons.io.DefaultBinarySerializer
+import org.beangle.commons.io.BinarySerializer
 import org.beangle.commons.net.http.{ HttpMethods, HttpUtils }
 import org.beangle.security.session.Session
 import org.beangle.security.session.cache.CacheSessionRepo
 
-class HttpSessionRepo(cacheManager: CacheManager) extends CacheSessionRepo(cacheManager) {
+class HttpSessionRepo(cacheManager: CacheManager, serializer: BinarySerializer)
+    extends CacheSessionRepo(cacheManager) {
 
   var geturl: String = _
   var accessUrl: String = _
 
   protected def getInternal(sessionId: String): Option[Session] = {
-    HttpUtils.getData(geturl.replace("{id}", sessionId)) map { is =>
-      DefaultBinarySerializer.deserialize(is, Map.empty).asInstanceOf[Session]
+    HttpUtils.getData(geturl.replace("{id}", sessionId)) map { data =>
+      serializer.asObject(classOf[Session], data)
     }
   }
 
