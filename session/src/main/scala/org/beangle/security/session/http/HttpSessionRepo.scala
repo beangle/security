53bbfcd8b9e4c717eb1dfdaa5144d72e594b4617
chaostone <duantihua@gmail.com>
2017-08-23 17:43:02 +0800

    Using Strings replace

--- session/src/main/scala/org/beangle/security/session/http/HttpSessionRepo.scala
+++ session/src/main/scala/org/beangle/security/session/http/HttpSessionRepo.scala
@@ -4,6 +4,7 @@ import java.net.{ HttpURLConnection, URL }
 
 import org.beangle.cache.CacheManager
 import org.beangle.commons.io.BinarySerializer
+import org.beangle.commons.lang.Strings.replace
 import org.beangle.commons.net.http.{ HttpMethods, HttpUtils }
 import org.beangle.security.session.Session
 import org.beangle.security.session.cache.CacheSessionRepo
@@ -15,14 +16,14 @@ class HttpSessionRepo(cacheManager: CacheManager, serializer: BinarySerializer)
   var accessUrl: String = _
 
   protected def getInternal(sessionId: String): Option[Session] = {
-    HttpUtils.getData(geturl.replace("{id}", sessionId)) map { data =>
+    HttpUtils.getData(replace(geturl, "{id}", sessionId)) map { data =>
       serializer.asObject(classOf[Session], data)
     }
   }
 
   def heartbeat(session: Session): Boolean = {
-    var surl = accessUrl.replace("{id}", session.id)
-    surl = surl.replace("{time}", session.lastAccessAt.getEpochSecond.toString)
+    var surl = replace(accessUrl, "{id}", session.id)
+    surl = replace(surl, "{time}", session.lastAccessAt.getEpochSecond.toString)
     val url = new URL(surl)
     val hc = url.openConnection().asInstanceOf[HttpURLConnection]
     hc.setRequestMethod(HttpMethods.GET)
