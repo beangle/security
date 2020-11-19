27e56db4dd902c9104efa8d462df270f8acc6a6c
段体华 <duantihua@gmail.com>
2020-11-19 20:16:54 +0800

    evict expired session first.

--- session/src/main/scala/org/beangle/security/session/cache/CacheSessionRepo.scala
+++ session/src/main/scala/org/beangle/security/session/cache/CacheSessionRepo.scala
@@ -33,21 +33,21 @@ abstract class CacheSessionRepo(val cacheManager: CacheManager)
   private val sessions = cacheManager.getCache("sessions", classOf[String], classOf[Session])
 
   /** 访问更新延迟
-   * 如果访问非常频繁，则根据访问延迟决定是否向后端发出心跳
-   * 默认按照20~60秒的随机数字防止更新过快,具体到后端需要看flushInterval
-   * 一般是3分钟
-   */
+    * 如果访问非常频繁，则根据访问延迟决定是否向后端发出心跳
+    * 默认按照20~60秒的随机数字防止更新过快,具体到后端需要看flushInterval
+    * 一般是3分钟
+    */
   private val accessDelaySeconds = new UpdateDelayGenerator(20, 60).generateDelaySeconds()
 
   protected val accessReporter = new AccessReporter(sessions, this)
 
   /**
-   * flush 间隔，以秒计,默认3分钟
-   */
+    * flush 间隔，以秒计,默认3分钟
+    */
   var flushInterval: Int = 3 * 60
 
   override def init(): Unit = {
-    SecurityDaemon.start("Beangle Session",flushInterval, this.accessReporter)
+    SecurityDaemon.start("Beangle Session", flushInterval, this.accessReporter)
   }
 
   override def get(sessionId: String): Option[Session] = {
@@ -72,6 +72,7 @@ abstract class CacheSessionRepo(val cacheManager: CacheManager)
           se
         } else {
           if (elapse == -1) {
+            evict(s)
             expire(s.id)
             None
           } else {
@@ -92,10 +93,9 @@ abstract class CacheSessionRepo(val cacheManager: CacheManager)
   protected def getInternal(sessionId: String): Option[Session]
 
   /** 向后端更新该会话的访问时间
-   *
-   * @param session 会话
-   * @return true如果是否依然存在该会话
-   */
+    * @param session 会话
+    * @return true如果是否依然存在该会话
+    */
   def flush(session: Session): Boolean
 
 }
