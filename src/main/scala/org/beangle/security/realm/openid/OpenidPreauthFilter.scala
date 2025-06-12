e84d2419c02dfb11d741b10d30415fc62867fa01
段体华 <duantihua@gmail.com>
2025-06-12 14:32:46 +0800

    support openid name checker

--- src/main/scala/org/beangle/security/realm/openid/OpenidPreauthFilter.scala
+++ src/main/scala/org/beangle/security/realm/openid/OpenidPreauthFilter.scala
@@ -18,7 +18,6 @@
 package org.beangle.security.realm.openid
 
 import jakarta.servlet.http.{HttpServletRequest, HttpServletResponse}
-import org.beangle.commons.bean.Initializing
 import org.beangle.commons.lang.Strings
 import org.beangle.commons.net.http.HttpUtils
 import org.beangle.security.authc.PreauthToken
@@ -29,23 +28,22 @@ import org.beangle.security.web.authc.AbstractPreauthFilter
  *
  * @param securityManager
  */
-class OpenidPreauthFilter(securityManager: WebSecurityManager) extends AbstractPreauthFilter(securityManager), Initializing {
+class OpenidPreauthFilter(securityManager: WebSecurityManager) extends AbstractPreauthFilter(securityManager) {
   var serviceUrl: String = _
-
-  override def init(): Unit = {
-    super.init()
-  }
 
   override protected def getCredential(req: HttpServletRequest): Option[Any] = {
     val p = req.getParameter("openid")
-    if Strings.isNotEmpty(p) then Some(p.trim) else None
+    val u = req.getParameter("username")
+    if Strings.isNotEmpty(p) && Strings.isNotBlank(u) then Some(p.trim) else None
   }
 
   override protected def resolveToken(req: HttpServletRequest, res: HttpServletResponse, credential: Any): Option[PreauthToken] = {
+    val username = req.getParameter("username")
     var url = serviceUrl
     url = Strings.replace(url, "{openid}", credential.toString)
+    url = Strings.replace(url, "{username}", username)
     val rs = HttpUtils.getText(url).getText.trim
-    if Strings.isBlank(rs) then None
-    else Some(new PreauthToken(rs, credential))
+    if Strings.isBlank(rs) && rs != "true" then None
+    else Some(new PreauthToken(username, credential))
   }
 }
