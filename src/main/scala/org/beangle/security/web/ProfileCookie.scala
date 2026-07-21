9f463e6992a2f65b223eefbbf5fe81c7ad239e2f
段体华 <duantihua@gmail.com>
2026-07-22 00:47:34 +0800

    add ProfileCookie

---
+++ src/main/scala/org/beangle/security/web/ProfileCookie.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2005, The Beangle Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.beangle.security.web
+
+import jakarta.servlet.http.{HttpServletRequest, HttpServletResponse}
+import org.beangle.commons.lang.Strings
+import org.beangle.security.authc.Profile
+import org.beangle.security.web.CookieKeys
+import org.beangle.web.servlet.util.CookieUtils
+
+object ProfileCookie {
+
+  def check(profiles: collection.Seq[Profile], profileId: String): Option[String] = {
+    if (Strings.isNotBlank(profileId)) {
+      val hit = profiles.exists(_.id.toString == profileId)
+      if (hit) return Some(profileId)
+    }
+    if (profiles.nonEmpty) Some(profiles.head.id.toString) else None
+  }
+
+  def add(request: HttpServletRequest, response: HttpServletResponse, profileId: String): Unit = {
+    CookieUtils.addCookie(request, response, CookieKeys.ProfileIdKey, profileId, "/", COOKIE_AGE)
+  }
+
+  /**
+   * 解析当前 profileId：优先 request 参数（profile / contextProfileId），否则读 cookie。
+   * 仅 contextProfileId 会写回 cookie（与历史行为一致）。
+   */
+  def get(request: HttpServletRequest, response: HttpServletResponse): Option[String] = {
+    val cookieId = Option(CookieUtils.getCookieValue(request, CookieKeys.ProfileIdKey)).filter(Strings.isNotBlank)
+
+    var pid = request.getParameter("profile")
+    if (Strings.isBlank(pid)) {
+      pid = request.getParameter("contextProfileId")
+      if (Strings.isBlank(pid)) {
+        cookieId
+      } else {
+        if (!cookieId.contains(pid)) {
+          ProfileCookie.update(request, response, pid, true)
+        }
+        Some(pid)
+      }
+    } else {
+      Some(pid)
+    }
+  }
+
+  def update(request: HttpServletRequest, response: HttpServletResponse, profileId: String, create: Boolean): Unit = {
+    Option(CookieUtils.getCookieValue(request, CookieKeys.ProfileIdKey)) match {
+      case Some(exists) =>
+        if (exists != profileId) {
+          add(request, response, profileId)
+        }
+      case None =>
+        if (create) {
+          add(request, response, profileId)
+        }
+    }
+  }
+
+  private val COOKIE_AGE = 60 * 60 * 24 * 7 // 7 days
+
+}
